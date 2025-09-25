dd3635e03ff45ec3c82f7027f6ccb0b1106c954d
kt5655-dev <kt5655@g.rit.edu>
2025-09-25 12:47:11 +0400

    first commit

---
+++ ff.py
@@ -0,0 +1,9 @@
+def trafc(light):
+    if lig == "green":
+        print ("cars can go")
+        
+    else:
+        print ("cars must stop")
+        
+lig = input("enter light color:")
+trafc(lig)
